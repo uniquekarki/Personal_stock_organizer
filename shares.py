eb73b03f4f5c84a4d215a06d5dff8ae15dc1c2ff
uniquekarki <unikarki@gmail.com>
2021-06-10 09:49:58 +0545

    First commit

---
+++ shares.py
@@ -0,0 +1,8 @@
+def calc_profit(quantity,purchase,sell):
+    return quantity*(sell - purchase)
+
+if __name__ == '__main__':
+    quantity = int(input("Enter the quantity of shares: "))
+    purchase = int(input("Enter the purchase price of your share: "))
+    sell = int(input("Enter the selling price of your share: "))
+    print("Your profit/loss is: Rs.", calc_profit(quantity,purchase,sell))
