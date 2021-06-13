10d08294b7411658a9b5e88eadae229f915fd03c
uniquekarki <unikarki@gmail.com>
2021-06-13 12:11:04 +0545

    Added input functionality

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from profit_calc import calc_profit
 
 if __name__ == '__main__':
-    quantity = int(input("Enter the quantity of shares: "))
-    purchase = int(input("Enter the purchase price of your share: "))
-    sell = int(input("Enter the selling price of your share: "))
-    print("Your profit/loss is: Rs.", calc_profit(quantity,purchase,sell))
+    n = None
+    print("\n")
+    print("----------------WELCOME TO YOUR PERSONAL STOCK ASSISTANT----------------")
+    print("\n")
+    while(n != 0):
+        print("Choose from the following:")
+        print("0. Exit")
+        print("1. Stock Calculator")
+        print("2. Add a stock to your list")
+        print("3. Remove the stock from your list")
+        print("4. Calculate your profit for a particular stock")
+        print("5. Calculate your total profit for today")
+        print("6. Show history")
+        n = int(input("Enter the number of choice: "))
+        print("\n")
+        if n == 1:
+            calc_profit()
--- profit_calc.py
+++ profit_calc.py
@@ -1,2 +1,15 @@
-def calc_profit(quantity,purchase,sell):
-    return quantity*(sell - purchase)
+def calc_profit():
+    n = None
+    while (n != 0):
+        print("Choose from the following:")
+        print("0. Exit")
+        print("1. For Calculating buy amount")
+        print("2. For Calculating sell amount")
+        n = int(input("Enter the number of choice: "))
+        print("\n")
+        if n ==2:
+            quantity = int(input("Enter the quantity of shares: "))
+            purchase = int(input("Enter the purchase price of your share: "))
+            sell = int(input("Enter the selling price of your share: "))
+            print("Your profit/loss is ----> Rs.", quantity*(sell - purchase),"\n")
+    return None
